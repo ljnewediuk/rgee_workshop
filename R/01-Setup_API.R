74ee0aee59c45c1a9aa2db91877a495a3d534102
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-04 10:15:47 -0700

    fix spacing, use TRUE

--- R/01-Setup_API.R
+++ R/01-Setup_API.R
@@ -13,7 +13,6 @@
 # able to login using your Google account on https://code.earthengine.google.com/
 
 # 2 - Set up rgee to interact with EE ====
-
 install.packages("rgee")
 
 # Required dependencies:
@@ -24,30 +23,30 @@ install.packages("rgee")
 #   Run the following to install Miniconda, set up Python environment, and 
 #   install the Earth Engine API and numpy:
 
-    library(rgee)
+library(rgee)
 
-    ee_install(py_env = "rgee")
+ee_install(py_env = "rgee")
 
 # Credentials dependencies:
 #   Not required, but recommended to move data from Earth Engine into your
 #   local R environment. Authentication allows access to 
 #   Google Drive/Cloud Storage. Only needs to be completed ONCE.
 #   
-#     Option 1: Initialize Earth Engine and Google Drive (recommend)
-      ee_Initialize(user = 'your_email_here@gmail.com', drive = T)
+#     Option 1: Initialize Earth Engine and Google Drive (recommended)
+ee_Initialize(user = 'your_email_here@gmail.com', drive = TRUE)
       
 #     When Google Drive is verified, you will be directed to an 
 #     authentication token. Copy the token and paste it in the R console.
       
 #     Option 2: Initialize Earth Engine and Google Cloud Storage
-      ee_Initialize(user = 'your_email_here@gmail.com', gcs = T)
+ee_Initialize(user = 'your_email_here@gmail.com', gcs = TRUE)
       
 #     Google Cloud Storage credentials must be set up manually at 
 #     https://code.markedmondson.me/googleCloudStorageR/articles/googleCloudStorageR.html
 #     
 #   When you're finished, check that everything worked properly:
 #   
-    ee_check_pyhton()
-    ee_check_credentials()
-    ee_check_python_packages()
+ee_check_python()
+ee_check_credentials()
+ee_check_python_packages()
 
